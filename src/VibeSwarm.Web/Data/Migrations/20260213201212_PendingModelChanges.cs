2db2c2bb1262bf0b6e9925d494d8bbea1f03f0ff
Kyle Weishaupt <kyleweishaupt@users.noreply.github.com>
2026-02-13 15:12:42 -0500

    Migration fix.

---
+++ src/VibeSwarm.Web/Data/Migrations/20260213201212_PendingModelChanges.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VibeSwarm.Shared.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IdeasAutoCommit",
+                table: "Projects",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdeasAutoCommit",
+                table: "Projects");
+        }
+    }
+}
